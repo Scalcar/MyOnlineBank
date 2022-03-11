a7631d916412fd20df3f79b4832c795b04cf6940
Scalcar <Scalcar@hotmail.com>
2022-03-11 16:29:44 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function(){
-    return view('first');
+    return view('First');
 }); 
 
 // Route::get('/dashboard', function () {
